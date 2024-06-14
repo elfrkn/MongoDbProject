bb386fa080c5723de3cdc8fb8791d1db09581241
Elif ÇALIŞ <pcengelf@hotmail.com>
2024-06-14 13:01:59 +0200

    .

--- MongoDbProject/Dtos/OrderDtos/CreateOrderDto.cs
+++ MongoDbProject/Dtos/OrderDtos/CreateOrderDto.cs
@@ -4,7 +4,7 @@
     {
         public int OrderProductPiece { get; set; }
         public DateTime OrderDate { get; set; }
-        public string CustomerId { get; set; }
+        public string? CustomerId { get; set; }
         public string? ProductId { get; set; }
     }
 }
--- MongoDbProject/Dtos/OrderDtos/ResultOrderDto.cs
+++ MongoDbProject/Dtos/OrderDtos/ResultOrderDto.cs
@@ -5,8 +5,7 @@
         public string OrderId { get; set; }
         public int OrderProductPiece { get; set; }
         public DateTime OrderDate { get; set; }
-        public string CustomerId { get; set; }
-
+        public string? CustomerId { get; set; }
         public string? ProductId { get; set; }
     }
 }
